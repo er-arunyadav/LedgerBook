6626bc8b616bb2c735a8fef826e0726e703d55e8
Arun Yadav <mrerarunyadav68@gmail.com>
2020-04-03 13:11:41 +0530

    second commit

---
+++ database/migrations/2020_04_03_072826_create_ledgers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLedgersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ledgers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('customer_id');
+            $table->string('date');
+            $table->string('particular');
+            $table->Integer('credit')->default(0);
+            $table->Integer('debit')->default(0);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ledgers');
+    }
+}
